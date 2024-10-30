8e423bfc85132b5ceae4a2f35518c578de41e7bb
Toni <51962051+EinToni@users.noreply.github.com>
2024-10-30 16:57:48 +0100

    feat(web): Synchronize albums from deduplicated images
    * Synchronization can be toggled
    * All images in the deduplication process will be added to all albums.
      Afterwards all images (if deleted or not) will be in the same albums.

--- web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -6,24 +6,24 @@
     notificationController,
   } from '$lib/components/shared-components/notification/notification';
   import DuplicatesCompareControl from '$lib/components/utilities-page/duplicates/duplicates-compare-control.svelte';
-  import type { AssetResponseDto } from '@immich/sdk';
+  import type { AssetResponseDto, AlbumResponseDto } from '@immich/sdk';
+  import { deleteAssets, updateAssets, getAllAlbums } from '@immich/sdk';
   import { featureFlags } from '$lib/stores/server-config.store';
   import { handleError } from '$lib/utils/handle-error';
-  import { deleteAssets, updateAssets } from '@immich/sdk';
   import { t } from 'svelte-i18n';
   import type { PageData } from './$types';
   import { suggestDuplicateByFileSize } from '$lib/utils';
   import LinkButton from '$lib/components/elements/buttons/link-button.svelte';
-  import { mdiCheckOutline, mdiTrashCanOutline } from '@mdi/js';
-  import { stackAssets } from '$lib/utils/asset-utils';
+  import { mdiCheckOutline, mdiTrashCanOutline, mdiKeyboard, mdiSync, mdiSyncOff } from '@mdi/js';
+  import { stackAssets, addAssetsToAlbum } from '$lib/utils/asset-utils';
   import ShowShortcuts from '$lib/components/shared-components/show-shortcuts.svelte';
   import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
-  import { mdiKeyboard } from '@mdi/js';
   import Icon from '$lib/components/elements/icon.svelte';
   import { locale } from '$lib/stores/preferences.store';
 
   export let data: PageData;
   export let isShowKeyboardShortcut = false;
+  let isSynchronizeAlbumsActive = true;
 
   interface Shortcuts {
     general: ExplainedShortcut[];
@@ -79,6 +79,9 @@
   const handleResolve = async (duplicateId: string, duplicateAssetIds: string[], trashIds: string[]) => {
     return withConfirmation(
       async () => {
+        if (isSynchronizeAlbumsActive) {
+          await synchronizeAlbums(duplicateAssetIds);
+        }
         await deleteAssets({ assetBulkDeleteDto: { ids: trashIds, force: !$featureFlags.trash } });
         await updateAssets({ assetBulkUpdateDto: { ids: duplicateAssetIds, duplicateId: null } });
 
@@ -89,6 +92,17 @@
       trashIds.length > 0 && !$featureFlags.trash ? $t('delete_duplicates_confirmation') : undefined,
       trashIds.length > 0 && !$featureFlags.trash ? $t('permanently_delete') : undefined,
     );
+  };
+
+  const synchronizeAlbums = async (assetIds: string[]) => {
+    const allAlbums: AlbumResponseDto[] = await Promise.all(
+      assetIds.map((assetId) => getAllAlbums({ assetId: assetId })),
+    );
+    const albumIds = [...new Set(allAlbums.flat().map((album) => album.id))];
+
+    albumIds.forEach((albumId) => {
+      addAssetsToAlbum(albumId, assetIds, false);
+    });
   };
 
   const handleStack = async (duplicateId: string, assets: AssetResponseDto[]) => {
@@ -171,6 +185,11 @@
       icon={mdiKeyboard}
       title={$t('show_keyboard_shortcuts')}
       on:click={() => (isShowKeyboardShortcut = !isShowKeyboardShortcut)}
+    />
+    <CircleIconButton
+      icon={isSynchronizeAlbumsActive ? mdiSync : mdiSyncOff}
+      title={$t('show_synchronise_albums')}
+      on:click={() => (isSynchronizeAlbumsActive = !isSynchronizeAlbumsActive)}
     />
   </div>
 
